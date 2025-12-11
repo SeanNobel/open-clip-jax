6b845ef3c94335c415f7f39a2dfb83b0adfec6df
SeanNobel <sensho19950503@gmail.com>
2025-12-11 18:20:07 +0900

    remove <=0.8.0 from flax requirement

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages, setup
 
 setup(
 	name='open_clip_jax',
-	version='0.0.2',
+	version='0.0.3',
 	description='Implementation of CLIP in JAX/Flax',
 	author='Borna Ahmadzadeh',
 	author_email='borna.ahz@gmail.com',
 	url='https://github.com/bobmcdear/open-clip-jax',
 	packages=find_packages(),
 	install_requires=[
-        'flax<=0.8.0',
+        'flax',
 		'huggingface_hub',
 		'optax',
 		'tensorflow',
