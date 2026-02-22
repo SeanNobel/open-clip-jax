e15afe1e6036046bcaf5b2d1fbba2f08dff258da
SeanNobel <sensho19950503@gmail.com>
2026-02-22 18:44:13 +0900

    update to 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name='open_clip_jax',
-	version='0.0.5',
+	version='0.0.6',
 	description='Implementation of CLIP in JAX/Flax',
 	author='Borna Ahmadzadeh',
 	author_email='borna.ahz@gmail.com',
